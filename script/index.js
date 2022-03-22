4fc11039c1db7cf73b81e3186af2ed2e12295c21
aina200 <ainapukipuki@gmail.com>
2022-03-22 16:13:54 +0100

    Class in progress

--- script/index.js
+++ script/index.js
@@ -1,4 +1,4 @@
-class RecipeCard {
+class ProjectCard {
 
 
     constructor(name, img, language,code,description) {
@@ -7,14 +7,15 @@ class RecipeCard {
         this.language = language;
         this.code = code;
         this.description = description;
-        
     }
 
+
     display() {
+
         const card = document.createElement("last_projects_box");
         card.innerHTML= `<div class="project_box">
             <div class="project_txt">
-                <h3>Les petits plats</h3>
+                <h3>${this.name}</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porta, tortor in cursus ullamcorper, ante risus convallis ipsum, ac molestie lectus arcu at risus. Phasellus a arcu scelerisque, laoreet nulla non, ultricies enim. Mauris ultricies erat sed lacus placerat, vitae pellentesque lacus blandit. Nullam id nisi eu libero consequat posuere ut at eros. Curabitur non lacus fermentum nisl lacinia suscipit vel eget ex. Phasellus ultrices metus vitae leo efficitur, vel ultrices purus pharetra. Fusce efficitur, justo eget rhoncus sodales, neque nisi lacinia neque, quis luctus lacus ligula quis purus. Proin massa arcu, fermentum nec felis eget, mattis lobortis orci. Praesent et quam a nulla varius facilisis.</p>
                 <h4>Languages utilisés: JS , HTML , CSS</h4>
                 <h5>Code Source</h5>
@@ -23,21 +24,19 @@ class RecipeCard {
                 <img src="/img/qualite-de-vie-equilibre.png" alt="">
             </div>
         </div>`
-
-
-
         return card;
     }
+
 
     
 }
 
-function photographerFactory(data) {
-    const photographersSection = document.querySelector(".last_projects");
-    const article = document.createElement( 'article' );
-    photographersSection.innerHTML="jh"
-    return {article}
-}
+// function photographerFactory(data) {
+//     const photographersSection = document.querySelector(".last_projects");
+//     const article = document.createElement( 'article' );
+//     photographersSection.innerHTML="jh"
+//     return {article}
+// }
 
 
 
@@ -51,8 +50,8 @@ function photographerFactory(data) {
 
     // console.log(getData)
     projects.forEach((project) => {
-        const photographerModel = photographerFactory(projects);
-        const userCardDOM = photographerModel.article;
+        const photographerModel = new ProjectCard();
+        const userCardDOM = photographerModel.display();
         photographersSection.appendChild(userCardDOM);
     });
 }
